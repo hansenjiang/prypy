7c45ccaa47760c8d58959ff55678918b7e555928
Hansen <hansen@sidratresearch.com>
2024-01-09 01:06:30 -0500

    initial commit

---
+++ src/prypy/models.py
@@ -0,0 +1,39 @@
+"""Objects representing notable Python code features.
+"""
+
+
+# Imports
+
+
+# Classes
+
+
+## Features
+
+
+class Function:
+    def __init__(self) -> None:
+        pass
+
+
+class Class:
+    def __init__(self) -> None:
+        pass
+
+
+class Constant:
+    def __init__(self) -> None:
+        pass
+
+
+class DataClass(Class):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+## PRY
+
+
+class Run:
+    def __init__(self) -> None:
+        pass
---
+++ src/prypy/reader.py
@@ -0,0 +1,14 @@
+"""Read Python scripts and identify objects.
+"""
+
+
+# Imports
+
+
+from pathlib import Path
+
+
+# Constants
+
+
+# Functions
