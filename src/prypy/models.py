6326c7f218f6fdf18b94d3da1e89280841d2b477
Hansen <hansen@sidratresearch.com>
2024-02-01 17:44:03 -0500

    [WIP] added some gobbledegook

--- src/prypy/models.py
+++ src/prypy/models.py
@@ -6,7 +6,7 @@
 
 
 from __future__ import annotations
-from typing import Any, Self, Union, Optional
+from typing import Any, Self
 from pathlib import Path
 import operator
 
@@ -24,8 +24,8 @@ class Variable:
         self,
         name: str,
         type: type,
-        value: Optional[Any],
-        parent: Union[Function, Class, Module],
+        value: Any | None,
+        parent: Function | Class | Module,
     ):
         self.name = name
         self.type = type
@@ -45,6 +45,21 @@ class Variable:
         constant_value = line.split("=")[1].strip()
         return Variable(constant_name, constant_type, constant_value, parent)
 
+    @classmethod
+    def from_signature(cls, signature: list[str], parent: Function) -> Self:
+        pass
+
+
+class Constant(Variable):
+    def __init__(
+        self, name: str, type: type, value: Any, parent: Function | Class | Module
+    ):
+        super().__init__(name, type, value, parent)
+
+    @classmethod
+    def from_line(cls, line: str, parent: Function | Class | Module):
+        pass
+
 
 class Function:
     def __init__(
@@ -54,7 +69,7 @@ class Function:
         output: list[Variable],
         calls: list[Self],
         lines: int,
-        parent: Union[Self, Class, Module],
+        parent: Self | Class | Module,
     ) -> None:
         self.name = name
         self.input = input
@@ -78,6 +93,27 @@ class Class:
         self.instance_attributes = instance_attributes
         self.functions = functions
         self.parent = parent
+
+    @classmethod
+    def from_lines(cls, lines: list[str], parent: Module) -> Self:
+        print(lines)
+        # Get name and create object
+        if "(" in lines[0]:
+            class_name = lines[0].split("class")[1].split("(")[0].strip()
+        else:
+            class_name = lines[0].split("class")[1].split(":")[0].strip()
+        class_obj = Class(class_name, [], [], [], parent)
+
+        # Skip signature
+        j = 0
+        while (not lines[j].strip()[-1] == ":") and (j < len(lines)):
+            j += 1
+
+        print(lines[: j + 1])
+
+        # Iterate over code block
+        while j < len(lines):
+            pass
 
 
 ## Package
@@ -103,8 +139,9 @@ class Module:
     @classmethod
     def from_path(cls, path: Path, parent: Package) -> Self:
         # TODO remove
-        print(path)
+        print("\n", path)
 
+        # Create object
         module = Module(path.stem, [], [], [], [], parent)
 
         # Store code as list[str] with trimmed newline character
@@ -112,7 +149,6 @@ class Module:
             line[:-1] if len(line) > 0 else line
             for line in open(path, mode="r").readlines()
         ]
-        tab_levels = [module]
 
         # Iterate over each line of code
         i = 0
@@ -165,47 +201,52 @@ class Module:
                     i += 1
                     continue
 
-            # Handle beginning of file - assume only imports and constants
-            if utils.tab_level(line) == 0:
-                # TODO implement imports
-                # Add imports
-                if "import " in line:
-                    if "from " in line:
-                        pass
-                    else:
-                        pass
+            # Encounter code blocks and statements at tab level 0
+            # TODO implement imports
+            # Add imports
+            if "import " in line:
+                if "from " in line:
+                    pass
+                else:
+                    pass
 
-                # Add constants
-                if ("=" in line) and ("(" not in line):
-                    module.constants.append(Variable.from_line(line, module))
+            # Add constants
+            elif ("=" in line) and ("(" not in line):
+                module.constants.append(Variable.from_line(line, module))
 
             # Add classes
-            elif "class " in line:
-                # If class inherits
-                if "(" in line:
-                    class_name = line.split("class")[1].split("(")[0].strip()
-                else:
-                    class_name = line.split("class")[1].split(":")[0].strip()
+            elif "class " in lines[i]:
+                # Find line number of end of definition
+                i_start = i
 
-                i += 1
-                line = lines[i]
-                while (
-                    (utils.tab_level(line) == 1)
-                    and (len(line.strip()) > 0)
-                    and (i < len(lines))
-                ):
+                # Skip signature
+                while not lines[i].strip()[-1] == ":":
                     i += 1
-                    line = lines[i]
 
-                #
-                tab_levels.append(class_name)
-                tab_levels = [module]
+                # Skip to line when tab level un-indents
+                while (
+                    (utils.tab_level(lines[i + 1]) == 1)
+                    or (utils.tab_level(lines[i + 2]) == 1)
+                ) and (i < len(lines) - 1):
+                    i += 1
+                i_end = i
+
+                # Create class
+                module.classes.append(
+                    Class.from_lines(lines[i_start : i_end + 1], module)
+                )
 
             # Add functions
             elif "def " in line:
                 pass
 
             i += 1
+
+        # Sort items by name
+        module.imports.sort(key=operator.attrgetter("name"))
+        module.constants.sort(key=operator.attrgetter("name"))
+        module.classes.sort(key=operator.attrgetter("name"))
+        module.functions.sort(key=operator.attrgetter("name"))
 
         return module
 
@@ -216,7 +257,7 @@ class Package:
         name: str,
         modules: list[Module],
         subpackages: list[Self],
-        parent: Union[Repository, Self],
+        parent: Repository | Self,
     ):
         self.name = name
         self.modules = modules
@@ -253,7 +294,7 @@ class Package:
         return pkg_str
 
     @classmethod
-    def from_path(cls, path: Path, parent: Union[Package, Repository]) -> Self:
+    def from_path(cls, path: Path, parent: Package | Repository) -> Self:
         package = Package(path.name, [], [], parent)
         item_paths = utils.get_item_paths(path)
 
@@ -285,10 +326,77 @@ class Package:
 
 
 class Repository:
-    def __init__(self, name: str, source: Package, path: Path):
+    """Python project repository containing a source package.
+
+    Parameters
+    ----------
+    name : str
+        Name of the repository.
+    path : Path
+        Path to the repository.
+    source : Package
+        Source python package containing all the modules of the project.
+
+    Methods
+    -------
+    from_path(path)
+        Generate a repository object from its path.
+    """
+
+    def __init__(self, name: str, path: Path, mode: str = "src"):
         self.name = name
-        self.source = source
         self.path = path
+
+        match mode:
+            # Assume repository structure of `[repo-name]/src/[repo_name]`
+            case "src":
+                if mode in utils.get_item_names(path):
+                    package_name = "_".join(path.name.split("-"))
+
+                    if package_name in utils.get_item_names((path / mode)):
+                        self.source = Package(
+                            package_name, (path / mode / package_name)
+                        )
+
+                    # Raise error if `repo_name` package not found
+                    else:
+                        raise ValueError(
+                            f"Expected source package {package_name} not found in repository {path.name}."
+                        )
+
+                # Raise error if source package not found
+                else:
+                    raise NotImplementedError(
+                        f"No source package discovered in repository {path.name}."
+                    )
+
+            # Raise error if unknown discovery mode
+            case _:
+                raise NotImplementedError(
+                    f"{format} is not a known package discovery format."
+                )
+
+        items = utils.get_item_names(path)
+
+        # Assume repository structure of `[repo-name]/src/[repo_name]`
+        src_name = "src"
+        if src_name in [item.name for item in items]:
+            src_items = utils.get_item_names((path / src_name))
+            package_name = "_".join(path.name.split("-"))
+
+            # Raise error if `repo_name` package not found
+            if package_name in src_items:
+                self.source = Package((path / src_name / package_name), repository)
+            else:
+                raise ValueError(
+                    f"Expected source package {package_name} not found in repository {path.name}."
+                )
+
+        # Raise error if source package not found
+        else:
+            raise NotImplementedError(
+                f"No source package discovered in repository {path.name}."
+            )
 
     def __str__(self) -> str:
         repo_str = f"\n══════════ {self.name} ══════════\n"
@@ -315,6 +423,8 @@ class Repository:
                 raise ValueError(
                     f"Expected source package {package_name} not found in repository {path.name}."
                 )
+
+        # Raise error if source package not found
         else:
             raise NotImplementedError(
                 f"No source package discovered in repository {path.name}."
@@ -322,6 +432,8 @@ class Repository:
 
         return repository
 
+
+# TODO remove test function
 
 if __name__ == "__main__":
     path = Path("/home/hansen/projects/gjw/morse/morse-pipeline").resolve()
--- src/prypy/utils.py
+++ src/prypy/utils.py
@@ -58,3 +58,105 @@ def tab_level(line: str) -> int:
         Number of indents to beginning of line content.
     """
     return max(len(line.split("\t")), len(line.split("    "))) - 1
+
+
+def is_encased(
+    line: str | list[str], pivot: str, left: str, right: str | None = None
+) -> list[bool]:
+    # Set right to left if not declared
+    if right is None:
+        right = left
+
+    # Set line to single line if multiple
+    if isinstance(line, list):
+        line = "".join(line)
+
+    # Raise error if `pivot` not in `line`
+    if pivot not in line:
+        raise ValueError(f"`{pivot}` not found in line `{line}`.")
+
+    # Create open/close status and pivot tracker
+    met = [[False, False] for i in range(len(line.split(pivot)) - 1)]
+    n_pivot = 0
+
+    # Read line from left to right
+    i = 0
+    while i < len(line):
+        if (i + len(pivot) - 1 < len(line)) and (line[i : i + len(pivot)] == pivot):
+            n_pivot += 1
+            if met[left]:
+                pass
+
+        if i + len(left) - 1 < len(line):
+            pass
+
+    # Check multiple lines
+    if isinstance(line, list):
+        pass
+    # Check single line
+    else:
+        #
+        segments = line.split(pivot)
+        i = 0
+        while i < len(segments) - 1:
+            # Not encased if `left` doesn't appear before `pivot`
+            if left not in segments[i]:
+                encased.append(False)
+            else:
+                pass
+            i += 1
+
+
+def remove_comments(lines: list[str]) -> list[str]:
+    # Iterate over all lines
+    i = 0
+    while i < len(lines):
+        # Remove single line comments
+        if "#" in lines[i]:
+            # Check each instance of "#"
+            j = 0
+            while j < len(lines[i].split("#")) - 1:
+                if lines[j][-1] == '"':
+                    pass
+                j += 1
+
+    # Skip empty lines and comments
+    if (len(line.strip()) == 0) or (line.strip()[0] == "#"):
+        i += 1
+        continue
+    # Skip multi-line strings
+    elif '"""' in line:
+        # If line is variable declaration, store if module-level constant
+        if (
+            ("=" in line.split('"""')[0])
+            and (line.split('"""')[0].split("=")[0].strip().isupper())
+            and (utils.tab_level(line) == 0)
+        ):
+            constant_name = line.split('"""')[0].split("=")[0].strip()
+            constant_str = line.split('"""')[1].strip()
+
+            # If only opening quotation mark is found, find closing mark
+            if len(line.split('"""')) == 2:
+                i += 1
+                line = lines[i]
+                while ('"""' not in line) and (i < len(lines)):
+                    constant_str += "\n" + line.strip()
+                    i += 1
+                    line = lines[i]
+                if len(line.split('"""')[0].strip()) > 0:
+                    constant_str += "\n" + line.strip()
+
+            # Create and store constant
+            module.constants.append(Variable(constant_name, str, constant_str, module))
+
+        # Otherwise skip string
+        else:
+            # If only opening quotation mark is found, find closing mark
+            if len(line.split('"""')) == 2:
+                i += 1
+                line = lines[i]
+                while ('"""' not in line) and (i < len(lines)):
+                    i += 1
+                    line = lines[i]
+            i += 1
+            continue
